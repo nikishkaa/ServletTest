0e361907be104b299551d274e578f5402f998826
nikishkaa <nikita.goutor@gmail.com>
2024-07-16 00:07:01 +0300

    Update.

---
+++ src/main/java/org/example/servlettest/Servlet.java
@@ -0,0 +1,17 @@
+package org.example.servlettest;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "", value = "/")
+public class Servlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+}
